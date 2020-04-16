295dba0f374ee3cd7f0667fcccaa454c633d220a
Chen Mo <mochen1228@gmail.com>
2020-04-16 06:56:10 -0400

    Added CORS

--- app.js
+++ app.js
@@ -31,6 +31,13 @@ const port = process.env.PORT || 4001;
 
 var app = express();
 app.use(express.static(path.join(__dirname,'client', 'build')));
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 });
