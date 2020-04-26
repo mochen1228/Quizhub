0793465de9058faace8c1292d3ac17a288ffba99
Chen Mo <mochen1228@gmail.com>
2020-04-25 22:55:02 -0400

    Added CORS

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(express.static(path.join(__dirname, 'client', 'build')));
 
 // CORS
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", "https://guarded-gorge-11703.herokuapp.com"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
